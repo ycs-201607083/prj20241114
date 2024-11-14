cc26ec02217d54d2360f90792f8ba019031af721
ycs-201607083 <nan0805@naver.com>
2024-11-14 12:33:37 +0900

    fe: 목록의 행 클릭 시 /view/:id로 이동

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,9 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Table } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -11,6 +13,10 @@ export function BoardList() {
       .then((res) => res.data)
       .then((data) => setBoardList(data));
   }, []);
+
+  function handleRowClick(id) {
+    navigate(`/view/${id}`);
+  }
 
   return (
     <Box>
@@ -26,9 +32,9 @@ export function BoardList() {
         </Table.Header>
         <Table.Body>
           {boardList.map((board) => (
-            <Table.Row key={board.id}>
+            <Table.Row onClick={() => handleRowClick(board.id)} key={board.id}>
               <Table.Cell>{board.id}</Table.Cell>
-              <Table.Cell>{board.content}</Table.Cell>
+              <Table.Cell>{board.title}</Table.Cell>
               <Table.Cell>{board.writer}</Table.Cell>
               <Table.Cell>{board.inserted}</Table.Cell>
             </Table.Row>
