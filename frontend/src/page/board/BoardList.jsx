cd5b718ded6c5c8b38c6cbd497ec878b58155019
ycs-201607083 <nan0805@naver.com>
2024-11-14 12:23:47 +0900

    front 게시물 목록 가져오기

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,15 +1,25 @@
 import { Box } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function BoardList() {
+  const [boardList, setBoardList] = useState([]);
+
   useEffect(() => {
-    axios.get("/api/board/list");
+    axios
+      .get("/api/board/list")
+      .then((res) => res.data)
+      .then((data) => setBoardList(data));
   }, []);
+
   return (
     <Box>
       <h3>게시물 목록</h3>
-      {/*게시물들을 테이블로 등록*/}
+      {boardList.map((board) => (
+        <li>
+          {board.title}, {board.writer}
+        </li>
+      ))}
     </Box>
   );
 }
