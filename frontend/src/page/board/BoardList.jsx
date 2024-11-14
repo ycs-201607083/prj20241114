ea53dc1d31e0ff347039d0422468354dafda2467
ycs-201607083 <nan0805@naver.com>
2024-11-14 12:28:45 +0900

    fe:게시물 목록 보기를 테이블로

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -15,11 +15,26 @@ export function BoardList() {
   return (
     <Box>
       <h3>게시물 목록</h3>
-      {boardList.map((board) => (
-        <li>
-          {board.title}, {board.writer}
-        </li>
-      ))}
+      <Table.Root interactive>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>번호</Table.ColumnHeader>
+            <Table.ColumnHeader>제목</Table.ColumnHeader>
+            <Table.ColumnHeader>작성자</Table.ColumnHeader>
+            <Table.ColumnHeader>작성일시</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {boardList.map((board) => (
+            <Table.Row key={board.id}>
+              <Table.Cell>{board.id}</Table.Cell>
+              <Table.Cell>{board.content}</Table.Cell>
+              <Table.Cell>{board.writer}</Table.Cell>
+              <Table.Cell>{board.inserted}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </Box>
   );
 }
