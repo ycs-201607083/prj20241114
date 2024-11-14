907372afa8b881800791a833e04426afccae4ba6
ycs-201607083 <nan0805@naver.com>
2024-11-14 10:14:52 +0900

    react 파일정리 필요없는 파일 삭제

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "./components/ui/provider.jsx";
+import { Toaster } from "./components/ui/toaster.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+      <Toaster />
+    </Provider>
   </StrictMode>,
-)
+);
