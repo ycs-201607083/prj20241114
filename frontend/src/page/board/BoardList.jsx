ae2da63bba6b17d1b927ece7fb028939b949b9a6
ycs-201607083 <nan0805@naver.com>
2024-11-14 12:12:32 +0900

    게시물 목록 보기 컴포넌트

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,9 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export function BoardList() {
+  useEffect(() => {
+    axios.get("/api/board/list");
+  }, []);
   return (
     <Box>
       <h3>게시물 목록</h3>
+      {/*게시물들을 테이블로 등록*/}
     </Box>
   );
 }
