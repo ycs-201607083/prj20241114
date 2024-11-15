51c2c45d9277222ee6f204457de08d85143c8455
ycs-201607083 <nan0805@naver.com>
2024-11-15 14:33:37 +0900

    조회된 결과 없을 대 테이블 대신 메시지 출력

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -86,26 +86,33 @@ export function BoardList() {
   return (
     <Box>
       <h3>게시물 목록</h3>
-      <Table.Root interactive>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>번호</Table.ColumnHeader>
-            <Table.ColumnHeader>제목</Table.ColumnHeader>
-            <Table.ColumnHeader>작성자</Table.ColumnHeader>
-            <Table.ColumnHeader>작성일시</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {boardList.map((board) => (
-            <Table.Row onClick={() => handleRowClick(board.id)} key={board.id}>
-              <Table.Cell>{board.id}</Table.Cell>
-              <Table.Cell>{board.title}</Table.Cell>
-              <Table.Cell>{board.writer}</Table.Cell>
-              <Table.Cell>{board.inserted}</Table.Cell>
+      {boardList.length > 0 ? (
+        <Table.Root interactive>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>번호</Table.ColumnHeader>
+              <Table.ColumnHeader>제목</Table.ColumnHeader>
+              <Table.ColumnHeader>작성자</Table.ColumnHeader>
+              <Table.ColumnHeader>작성일시</Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {boardList.map((board) => (
+              <Table.Row
+                onClick={() => handleRowClick(board.id)}
+                key={board.id}
+              >
+                <Table.Cell>{board.id}</Table.Cell>
+                <Table.Cell>{board.title}</Table.Cell>
+                <Table.Cell>{board.writer}</Table.Cell>
+                <Table.Cell>{board.inserted}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      ) : (
+        <p>검색 결과가 없습니다.</p>
+      )}
 
       <HStack>
         <NativeSelectRoot
