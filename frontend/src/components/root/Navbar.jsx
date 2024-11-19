77cb630b268918c8faba30d31a3b7cfde8110d53
ycs-201607083 <nan0805@naver.com>
2024-11-19 14:05:49 +0900

    임시로 로그인 한 사용자 출력

--- frontend/src/components/root/Navbar.jsx
+++ frontend/src/components/root/Navbar.jsx
@@ -1,8 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
+import { jwtDecode } from "jwt-decode";
 
 export function Navbar() {
   const nav = useNavigate();
+
+  //TODO: 임시.. 삭제할 에정
+  const token = localStorage.getItem("token");
+  let name;
+  if (token) {
+    const decode = jwtDecode(token);
+    name = decode.sub;
+  }
 
   return (
     <Flex gap={3}>
@@ -19,6 +28,7 @@ export function Navbar() {
       >
         로그아웃
       </Box>
+      <Box>{name}</Box>
     </Flex>
   );
 }
