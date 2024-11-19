5eefdef45be17067643f220f76496fdb2b171720
ycs-201607083 <nan0805@naver.com>
2024-11-19 10:57:04 +0900

    Spring Security 설정

---
+++ backend/src/main/java/com/example/backend/config/AppConfiguration.java
@@ -0,0 +1,51 @@
+package com.example.backend.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+@EnableMethodSecurity
+public class AppConfiguration {
+
+    @Value("classpath:secret/app.pub")
+    RSAPublicKey pub;
+    @Value("classpath:secret/app.key")
+    RSAPrivateKey priv;
+
+    @Bean
+    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(c -> c.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(pub).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(pub).privateKey(priv).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwks);
+    }
+}
