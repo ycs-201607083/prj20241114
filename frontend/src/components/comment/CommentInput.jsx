7f949f081d14df6f5c7c7645af87fbe36c142964
ycs-201607083 <nan0805@naver.com>
2024-11-20 15:20:02 +0900

    로그인 되어 있을 경우에만 댓글 작성 가능

--- frontend/src/components/comment/CommentInput.jsx
+++ frontend/src/components/comment/CommentInput.jsx
@@ -1,18 +1,23 @@
 import { Box, Group, Textarea } from "@chakra-ui/react";
 import { Button } from "../ui/button.jsx";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
 
 export function CommentInput({ boardId, onSaveClick }) {
   const [comment, setComment] = useState("");
+  const { isAuthenticated } = useContext(AuthenticationContext);
   return (
     <Box>
       <Group>
         <Textarea
           value={comment}
+          disabled={!isAuthenticated}
+          placeholder={isAuthenticated ? "" : "로그인 후 작성가능"}
           onChange={(e) => setComment(e.target.value)}
           variant={"outline"}
         />
         <Button
+          disabled={!isAuthenticated}
           onClick={() => {
             onSaveClick(comment);
             setComment("");
